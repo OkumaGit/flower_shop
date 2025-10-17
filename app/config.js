147dd5f001ca93890e6b7a86f834eb1c5fda1ff1
Okuma <veryeasy30@gmail.com>
2025-10-17 19:42:56 +0200

    fix URL API env

--- app/config.js
+++ app/config.js
@@ -1,7 +1,8 @@
 const config = {
   API_URL:
-    window.location.hostname == "localhost"
-      ? "http://localhost:4000/"
+    window.location.hostname == "localhost" ||
+    window.location.hostname === "127.0.0.1"
+      ? "http://localhost:4000"
       : "https://flower-shop-ld2e.onrender.com",
 };
 
--- app/modules/sendOrder.js
+++ app/modules/sendOrder.js
@@ -1,4 +1,4 @@
-import config from "../config";
+import config from "../config.js";
 
 const sendOrder = async function sendOrder(orderData) {
   console.log("sendOrder function called");
