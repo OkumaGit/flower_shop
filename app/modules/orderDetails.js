4a276bfa902237ab39ff6ff8e362452c011a4b39
Okuma <veryeasy30@gmail.com>
2026-02-02 08:05:36 +0100

    before auth

--- app/modules/orderDetails.js
+++ app/modules/orderDetails.js
@@ -3,11 +3,18 @@ let fromLocalStorage;
 let fromLocalStorageOrderedItems;
 let renderDetailsTo;
 
+// ORDER DETAILS
+let orderedItems = document.createElement("div");
+orderedItems.className = "col s6 m12 card horizontal";
+orderedItems.style.display = "flex";
+orderedItems.style.flexDirection = "column";
+//
+
 const orderDetails = () => {
-  // PULL NEEDED DATA
   document.addEventListener("DOMContentLoaded", function () {
     renderDetailsTo = document.getElementById("fromContactForm");
     renderTo = document.getElementById("orderedProducts");
+    // PULL NEEDED DATA
     fromLocalStorage = localStorage.getItem("inCartFlowerData")
       ? JSON.parse(localStorage.getItem("inCartFlowerData"))
       : console.log("Local storage was empty");
@@ -15,14 +22,14 @@ const orderDetails = () => {
       localStorage.getItem("orderData")
     );
     console.log("OrderData: ", fromLocalStorageOrderedItems);
+    //
 
-    // RENDERING WITH FOREACH
-
+    // RENDERING PRODUCTS FROM ORDER WITH IMAGES
     fromLocalStorage.forEach((data) => {
       let toRender = document.createElement("div");
       toRender.className = "col s6 m6";
       toRender.innerHTML = `<div class="card horizontal">
-      <div class="card-image" style="max-width: 33%">
+      <div class="card-image" style="max-width: 22%">
         <img class="cardImage" src="${data.Image}" />
       </div>
       <div class="card-stacked">
@@ -46,21 +53,20 @@ const orderDetails = () => {
     });
     //
 
-    // RENDERING WITH FOREACH
+    // RENDERING ORDER DETAILS
     Object.entries(fromLocalStorageOrderedItems).forEach((data) => {
       let orderedItem = document.createElement("div");
-      console.log("data :", data);
       if (data[0] != "items") {
         if (data[0] == "summPrice") {
           orderedItem.innerHTML += `<div style="display: flex; align-items: center; width: 100%">
       <div>${data[0]}</div>
-      <div style="line-height: 1; border-bottom: 1px dashed #000; height: 1em; margin: 16px; flex: 1 1 auto"></div>
+      <div style="line-height: 1; border-bottom: 1px dashed #000; height: 1em; margin: 8px 16px; flex: 1 1 auto"></div>
       <div class = "bold" style="font-weight: 700">$${data[1]}</div>
     </div>`;
         } else {
           orderedItem.innerHTML += `<div style="display: flex; align-items: center; width: 100%">
       <div>${data[0]}</div>
-      <div style="line-height: 1; border-bottom: 1px dashed #000; height: 1em; margin: 16px; flex: 1 1 auto"></div>
+      <div style="line-height: 1; border-bottom: 1px dashed #000; height: 1em; margin: 8px 16px; flex: 1 1 auto"></div>
       <div>${data[1]}</div>
     </div>`;
         }
@@ -69,12 +75,13 @@ const orderDetails = () => {
     });
     //
 
-    // RENDERING ITEMS FROM Array.Object
+    // FINAL RENDER ITEMS FROM Array.Object
     let products = document.createElement("div");
     products.className = "col s6 m12 card horizontal orderedItems";
-
+    // First section
     fromLocalStorageOrderedItems.items.forEach((item) => {
-      products.innerHTML += `<div style="padding-right: 35px;"><div>${item.Name}</div>
+      products.innerHTML += `<div class="card-image" style="padding-right: 35px; max-width: 11%"><img class="cardImage" src="${item.Image}" />
+      <div style="padding-right: 35px;"><div>${item.Name}</div>
     <div>x${item.Qty}</div>
     <div>$${item.Price}</div></div>`;
     });
@@ -82,11 +89,6 @@ const orderDetails = () => {
     renderDetailsTo.appendChild(orderedItems);
   });
   //
-
-  // ORDER DETAILS
-  let orderedItems = document.createElement("div");
-  orderedItems.className = "col s6 m12 card horizontal";
-  orderedItems.style.display = "flex";
-  orderedItems.style.flexDirection = "column";
 };
+
 export { orderDetails };
